055e66255d96f24ec2f533fb7a8b78257180844e
Shannon McNellis <shannonmcnellis@Shannons-MacBook-Pro.local>
2017-02-07 14:43:31 -0500

    initial commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,13 @@
 console.log('wat')
+
+import $ from 'jquery';
+
+
+$.getJSON('')
+
+var forEach = function(arr,cb){for(var i = 0; i < arr.length; i++){ cb(arr[i], i, arr)  } }
+
+// function controllerRouter (){
+//   var currentRoute= window.location.hash
+//   console.log(currentRoute)
+// }
